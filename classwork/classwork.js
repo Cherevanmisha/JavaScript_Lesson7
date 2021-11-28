af67831ebf49f1354eba4a2991f8afa65468576d
Cherevanmisha <cherevanmisha@ukr.net>
2021-11-28 22:16:25 +0200

    Lesson7

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -8,49 +8,60 @@
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів,
 // і додає його в поточний об'єкт car
 
-
-let car=
-    {
-model: 'Mazda',
-year: 2021,
-producer: 'Mazda Motor Corporation',
-tankVolume: '51 liters',
-horsePower: '187 hp',
-maxspeed: '240 km',
-nameDraiver: 'Artur',
-
-
-  drive: function(){
-    console.log(`Їдемо зі швидкістю ${this.maxspeed} на годину`);
-},
-
- info: function() {
-    console.log(`model: ${this.model} producer: ${this.producer} year: ${this.year} nameDraiver: ${this.nameDraiver}
-    tankVolume: ${this.tankVolume} horsePower: ${this.horsePower} maxspeed: ${this.maxspeed}`)
-},
-
-increaseMaxSpeed: function  (newSpeed){
-		this.maxspeed += newSpeed;
-	},
-	changeYear: function (newValue){
-		this.year = newValue;
-	},
-
-	addDriver: function (driver){
-		this.nameDraiver = carDriver;
-	}
-};
- let carDriver = {
-    	name:'Borus',
-    };
-
-
-car.info();
-car.drive();
-car.increaseMaxSpeed();
-car.changeYear();
-car.addDriver();
-
+//
+// let cars=
+// 	{
+// 		model: 'Mazda',
+// 		year: 2021,
+// 		producer: 'Mazda Motor Corporation',
+// 		tankVolume: '51 liters',
+// 		horsePower: '187 hp',
+// 		maxspeed: '240 km',
+// 		nameDraiver: 'Artur',
+//
+// 	};
+// let driver ={name: 'Artur',age:58 };
+// function Car (model,year,producer,tankVolume,horsePower,maxspeed,nameDraiver){
+// 	this.model = model || 'Mazda';
+// 	this.year = year || 2021;
+// 	this.producer = producer || 'Mazda Motor Corporation';
+// 	this.tankVolume = tankVolume || '51 liters';
+// 	this.horsePower = horsePower || '187 hp';
+// 	this.maxspeed = maxspeed ||  '240 km';
+// 	this.nameDraiver = nameDraiver || 'Artur';
+//
+// 	this.drive= function(){
+// 		console.log(`Їдемо зі швидкістю ${this.maxspeed} на годину`);
+// 	},
+//
+// 		this.info=function() {
+// 			console.log(`model: ${this.model} producer: ${this.producer} year: ${this.year} nameDraiver: ${this.nameDraiver}
+//     tankVolume: ${this.tankVolume} horsePower: ${this.horsePower} maxspeed: ${this.maxspeed}`)
+// 		},
+// 		this.increaseMaxSpeed= function (newSpeed){
+// 			this.maxspeed += newSpeed;
+// 			console.log(newSpeed)
+// 		},
+// 		this.changeYear=function (newValue){
+// 			this.year = newValue;
+// 			console.log(newValue)
+// 		},
+//
+// 		this.addDriverthis=function (driver){
+// 			this.nameDraiver = driver;
+// 			console.log(driver);
+//
+// 		}
+// };
+//
+//
+// let car = new Car('Volvo', 2017)
+//
+// car.info();
+// car.drive();
+// car.increaseMaxSpeed(170);
+// car.changeYear(2025);
+// car.addDriverthis('Borus');
 
 
 
@@ -63,7 +74,42 @@ car.addDriver();
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 
 
+class Cars {
+	constructor(model, year, producer, tankVolume, horsePower, maxspeed, nameDraiver) {
+		this.model = model;
+		this.year = year;
+		this.producer = producer;
+		this.tankVolume = tankVolume;
+		this.horsePower = horsePower;
+		this.maxspeed = maxspeed;
+		this.nameDraiver = nameDraiver;
+	}
 
+	drive() {
+		console.log(`Їдемо зі швидкістю ${this.maxspeed} на годину`);
+	}
+
+	info(){
+		console.log(`model: ${this.model} producer: ${this.producer} year: ${this.year} 
+		nameDraiver: ${this.nameDraiver}
+    tankVolume: ${this.tankVolume} horsePower: ${this.horsePower} maxspeed: ${this.maxspeed}`)
+	}
+	increaseMaxSpeed(newSpeed) {
+		this.maxspeed += newSpeed;
+	}
+	changeYear(newValue) {
+		this.year = newValue;
+	}
+	addDriver(nameDraiver) {
+		this.nameDraiver = nameDraiver;
+	}
+}
+
+car.info();
+car.drive();
+car.increaseMaxSpeed();
+car.changeYear();
+car.addDriver();
 
 
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
