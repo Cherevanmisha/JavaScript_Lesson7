3b8a64962a848fa6c3a74fa938cbf49d511c353b
Cherevanmisha <cherevanmisha@ukr.net>
2021-11-30 20:54:46 +0200

    Lesson7

--- additional/additional.js
+++ additional/additional.js
@@ -4,25 +4,25 @@
 // в т.ч і для властивостей внутрішніх об'єктів
 
 
-function con (id, name, username,email,street,suite,city,zipcode,lat,lng,phone,website,name1,catchPhrase,bs) {
-    this.id = id;
-    this.name = name;
-    this.username = username;
-    this.email = email;
-    this.address = {street: street, suite: suite, city: city, zipcode: zipcode};
-    this.geo = {lat: lat, lng: lng};
-    this.phone = phone;
-    this.website = website;
-    this.company ={name1:name1,catchPhrase:catchPhrase,bs:bs}
-
-
-
-
-}
-let con1 = new con(1,'Leanne Graham','Bret','Sincere@april.biz','Kulas Light',
-    'Apt. 556','Gwenborough','92998-3874','-37.3159','81.1496','1-770-736-8031 x56442',
-    'hildegard.org','Romaguera-Crona','Multi-layered client-server neural-net','harness real-time e-markets');
-console.log(con1);
+// function con (id, name, username,email,street,suite,city,zipcode,lat,lng,phone,website,name1,catchPhrase,bs) {
+//     this.id = id;
+//     this.name = name;
+//     this.username = username;
+//     this.email = email;
+//     this.address = {street: street, suite: suite, city: city, zipcode: zipcode};
+//     this.geo = {lat: lat, lng: lng};
+//     this.phone = phone;
+//     this.website = website;
+//     this.company ={name1:name1,catchPhrase:catchPhrase,bs:bs}
+//
+//
+//
+//
+// }
+// let con1 = new con(1,'Leanne Graham','Bret','Sincere@april.biz','Kulas Light',
+//     'Apt. 556','Gwenborough','92998-3874','-37.3159','81.1496','1-770-736-8031 x56442',
+//     'hildegard.org','Romaguera-Crona','Multi-layered client-server neural-net','harness real-time e-markets');
+// console.log(con1);
 
 
 // {
